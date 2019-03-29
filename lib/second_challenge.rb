f8bf80c842e7910a54cc25f53cb17b9541b1b8da
Angela Mumba <malaika.mumba@gmail.com>
2019-03-29 23:02:15 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,10 +1,19 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
+      #your key/value pairs here! first 3 point to value of an empty hash , the fourth has the value of an empty array. 
+      :patriarch => {}
+      :matriarch => {}
+      :hero => {}
+      :hero_friends =>[]
       }, 
     :capulet => {
       #your key/value pairs here!
+      
+      :patriarch => {}
+      :matriarch => {}
+      :heroin => {}
+      :heroin_friends =>[]
       }}
 
   
